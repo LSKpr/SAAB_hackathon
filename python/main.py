1b14ee3773964d69a05029967635dad771744561
jkluge <jkluge@kth.se>
2026-04-25 11:23:01 +0200

    Python template

---
+++ python/main.py
@@ -0,0 +1,11 @@
+# open a webserver to handle data to server 
+
+# lists of: 
+#   currentThreats (concurrentList)
+#   potentialTargets (infrastructure or bases)
+#   potentialCountermeasures 
+
+
+# Please make new files for things that can be encapsulated :))
+
+# goal: getImportanceLevelForThreat()
